be35080eb36318f815aa3ace4d9f58b1c85f6978
Yehor <eee.houmich@gmail.com>
2024-04-01 21:07:24 +0300

    make build

---
+++ index.6d516611.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(r){r.preventDefault();var t=r.target.closest("a");t&&(e.src=t.href)});
+//# sourceMappingURL=index.6d516611.js.map
